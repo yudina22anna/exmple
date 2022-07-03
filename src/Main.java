a2da55f99a2f3e12d27d3be20d8d7217610a5ad6
annayudina <yudina.anna.zi15@gmail.com>
2022-07-03 14:11:58 +0300

    exercise1

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "3534228227938012";
-        System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
-    }
-
-    public static boolean isValidCardNumber(String number) {
-        if (number == null) {
-            return false;
-        }
-
-        if (number.length() != 16) {
-            return false;
-        }
-
-        long result = 0;
-        for (int i = 0; i < number.length(); i++) {
-            int digit;
-            try {
-                digit = Integer.parseInt(number.charAt(i) + "");
-            } catch (NumberFormatException e) {
-                return false;
-            }
-
-            if (i % 2 == 0) {
-                digit *= 2;
-                if (digit > 9) {
-                    digit -= 9;
-                }
-            }
-            result += digit;
-        }
-
-        return (result != 0) && (result % 10 == 0);
+        int currentBalance = 2_000_000_000;
+        int transferAmount = 500_000_000;
+        int finalValue = currentBalance+transferAmount;
+        System.out.println(finalValue);
     }
 }
